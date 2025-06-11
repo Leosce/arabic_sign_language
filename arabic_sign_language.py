679dbbf809643ad9707b896885fb18227eaea0e0
Leosce <121965527+Leosce@users.noreply.github.com>
2025-06-11 06:00:54 +0300

    Update arabic_sign_language.py

--- arabic_sign_language.py
+++ arabic_sign_language.py
@@ -11,7 +11,7 @@ import numpy as np
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Load YOLO model once
-model = YOLO(r"C:\Users\user\arabic_signs\Models\model.pt")
+model = YOLO(r"model.pt")
 model.fuse()  # Optimize model for inference
 
 st.title("🎯 Real-Time Arabic Language Sign Detection with YOLOv11n!")
